f830b77f29f40b6faf3818c10d952523fab39ae7
mh07607 <mh07607@st.habib.edu.pk>
2023-04-19 00:20:23 +0500

    merkle_proof implementation
    -merkle_proof function implemented
    -additional attributes defined for Node class for ease of merkle_proof
    -additional code in build_tree_rec for ease of merkle_proof

--- src/merkle_tree.py
+++ src/merkle_tree.py
@@ -16,10 +16,12 @@ class Node:
 		self.value = value
 		self.content = content
 		self.is_copied = is_copied
-		
+		self.is_left = None
+		self.parent: Node = None
+		self.leaves = []
+
 	@staticmethod
 	def hash(val: str) -> str:
-		# Calculate the SHA-256 hash of a string.
 		# val (str): The input string to hash.
 		# The SHA-256 hash of the input string.
 
@@ -31,6 +33,15 @@ class Node:
 	def copy(self):
 		#creates and returns the copy of a node. 
 		return Node(self.left, self.right, self.value, self.content, True)
+	
+	def neighbour(self): 
+		if(self.is_left== 1):
+			return self.parent.right
+		else:
+			return self.parent.left
+
+	def set_parent(self, parent):
+		self.parent = parent
 	
 
 class MerkleTree:  # Defining the MerkleTree class
@@ -45,6 +56,7 @@ class MerkleTree:  # Defining the MerkleTree class
 		#Build the Merkle Tree recursively.
 		# Create leaf nodes for each value in the input list
 		leaves: List[Node] = [Node(None, None, Node.hash(e), e) for e in values]
+		self.leaves = leaves
 		# If the number of leaves is odd, duplicate the last leaf
 		if len(leaves) % 2 == 1:
 			leaves.append(leaves[-1].copy()) # duplicate last elem if odd number of elements
@@ -62,16 +74,29 @@ class MerkleTree:  # Defining the MerkleTree class
 		if len(nodes) == 2:
 			Hash_Value = Node.hash(nodes[0].value + nodes[1].value)
 			Combined_Content = nodes[0].content+"+"+nodes[1].content
-			return Node(nodes[0], nodes[1], Hash_Value, Combined_Content)
+			parent = Node(nodes[0], nodes[1], Hash_Value, Combined_Content)
+
+			nodes[0].is_left = 1
+			nodes[0].parent = parent
+
+			nodes[1].is_left = 0
+			nodes[1].parent = parent
+
+			return parent
 		
 		# Recursively build the left and right subtrees
 		left: Node = self.__buildTreeRec(nodes[:half])
 		right: Node = self.__buildTreeRec(nodes[half:])
-
+		left.is_left = 1
+		right.is_left = 0
 
 		value: str = Node.hash(left.value + right.value)  #Calculates Hash
-		content: str = f'{left.content}+{right.content}' #Storing Content for testing purposes
-		return Node(left, right, value, content) #Returns The root node of the Merkle Tree.
+		content: str = f'{left.content}+{right.content}'
+		
+		parent = Node(left, right, value, content) 
+		left.parent = parent
+		right.parent = parent #Storing Content for testing purposes
+		return parent #Returns The root node of the Merkle Tree.
 
 	def printTree(self) -> None:
 		self.__printTreeRec(self.root) #Calling Helper function
@@ -98,9 +123,32 @@ class MerkleTree:  # Defining the MerkleTree class
 			self.__printTreeRec(node.left)
 			self.__printTreeRec(node.right)
 
+	def merkle_proof(self, node:Node):
+		path = []
+		hashed_value = Node.hash(node.content)
+
+		while(node.parent != None):
+			path.append(node.content)
+			hashed_value = Node.hash(hashed_value + node.neighbour().value)
+			# if(hashed_value != node.parent.value):
+			# 	return False
+			node.parent.value
+			node = node.parent
+
+		#hashed_value = Node.hash(str(node.value) + str(node.neighbour().value))
+		path.append(node.content)
+		#path.append(self.root.content)
+
+		if(self.root.value != hashed_value):
+			print(node.content)
+			return False
+		return path
+
+
 	def getRootHash(self) -> str:
 	    return self.root.value  #Returns The SHA-256 hash of the root node of the Merkle Tree.
-
+	
+	
 def mixmerkletree() -> None:
 	""" The mixmerkletree() function takes a list of transactions and generates a Merkle tree by 
 	recursively combining pairs of hashes until only one root hash remains. It then adds a 
@@ -109,7 +157,7 @@ def mixmerkletree() -> None:
 	original transaction from the final output. """
 
 	# Define a list of input values
-	elems = ["Lorem Impsum Mahio", ""]
+	elems = ["a", "b", "c", "d"]
 	#As there are odd number of inputs, the last input is repeated
 	print("Inputs: ")
 	print(*elems, sep=" | ") # Print the input values separated by "|"
@@ -117,6 +165,8 @@ def mixmerkletree() -> None:
 	mtree = MerkleTree(elems) # Create a Merkle Tree from the input values
 	print("Root Hash: "+mtree.getRootHash()+"\n") # Print the root hash of the Merkle Tree
 	mtree.printTree() # Print the entire Merkle Tree
+	mtree.leaves[0].content = 'a'
+	print(mtree.merkle_proof(mtree.leaves[0]))
 
 mixmerkletree()
 
